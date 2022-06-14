22b54557ef853328feef9eb7bcbb23739c58be23
marcosricciardi12 <marcosricciardi12@gmail.com>
2022-06-14 11:32:20 -0300

    Ult actualizacion

--- automatas/clearscreen copy.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def clearscreen():
-        if os.name == "posix":
-            os.system ("clear")
-        elif os.name == "ce" or os.name == "nt" or os.name == "dos":
-            os.system ("cls")
